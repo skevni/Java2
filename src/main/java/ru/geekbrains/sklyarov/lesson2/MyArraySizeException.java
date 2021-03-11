0cc08c7079174d5c180a969cca2dfbb16bad366a
Evgeniy Sklyarov <skevni@gmail.com>
2021-03-11 22:11:20 +0300

    for main branch

--- src/main/java/ru/geekbrains/sklyarov/lesson2/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.sklyarov.lesson2;
-
-public class MyArraySizeException extends ArrayIndexOutOfBoundsException{
-    public MyArraySizeException(String s) {
-        super(s);
-    }
-}
